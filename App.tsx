6c5f4d82acc5a358333d199dec8d085318c93a5d
Yue Chang <r3399r@gmail.com>
2021-09-21 16:24:57 +0800

    add linter, prettier, and husky

--- App.tsx
+++ App.tsx
@@ -2,14 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => (
+  <View style={styles.container}>
+    <Text>Open up App.tsx to start working on your app!</Text>
+    <StatusBar style="auto" />
+  </View>
+);
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
